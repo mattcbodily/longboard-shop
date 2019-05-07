2340828f7631fbaba0f7257b673efe239c21dea2
Matthew Bodily <matthewbodily@Matthews-MacBook-Air.local>
2019-05-07 13:38:22 -0600

    added the basic structure of redux for future use

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import {HashRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import routes from './routes';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import store from './ducks/store';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <HashRouter>
-        <Header />
-          {routes}
-        <Footer />
-      </HashRouter>
+      <Provider store = {store}>
+        <HashRouter>
+          <Header />
+            {routes}
+          <Footer />
+        </HashRouter>
+      </Provider>
     </div>
   )
 }
