4a11a7327a590ac05ab7360dd5accd5964135338
Matthew Bodily <matthewbodily@Matthews-MacBook-Air.local>
2019-05-21 12:39:51 -0600

    beginning setup in the reducer

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,27 @@
 const initialState = {
-    
+    design: {
+        name: '',
+        size: '',
+        image: '',
+        price: 0.00
+    },
+    grip: {
+        name: '',
+        image: '',
+        price: 0.00
+    },
+    trucks: {
+        name: '',
+        image: '',
+        price: 0.00
+    },
+    wheels: {
+        color: '',
+        image: '',
+        price: 0.00
+    },
+    graphic: '',
+    total: 0.00
 }
 
 export default function reducer(state = initialState, action){
