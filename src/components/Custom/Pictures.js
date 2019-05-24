0f36a45234c814b8d895597604420e9d73ac25ae
Matthew Bodily <mattcbodily@gmail.com>
2019-05-24 16:32:16 -0600

    users can now see their uploaded image, styling has been adjusted in the pictures component

--- src/components/Custom/Pictures.js
+++ src/components/Custom/Pictures.js
@@ -4,7 +4,6 @@ import {connect} from 'react-redux';
 import axios from 'axios';
 import {v4 as randomString} from 'uuid';
 import Dropzone from 'react-dropzone';
-import {GridLoader} from 'react-spinners';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
 import {updateGraphic} from './../../ducks/reducer';
@@ -13,8 +12,7 @@ class Pictures extends Component {
     constructor(props){
         super(props);
         this.state = {
-            isUploading: false,
-            url: ''
+            isUploading: false
         }
     }
 
@@ -48,7 +46,7 @@ class Pictures extends Component {
         };    
         axios.put(signedRequest, file, options)
         .then(response => {
-            this.setState({ isUploading: false, url })
+            this.setState({ isUploading: false})
             this.handleGraphic(url)
         })
         .catch(err => {
@@ -56,19 +54,17 @@ class Pictures extends Component {
               isUploading: false,
             });
             if (err.response.status === 403) {
-              alert(
-                `Your request for a signed URL failed with a status 403. Double check the CORS configuration and bucket policy in the README. You also will want to double check your AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in your .env and ensure that they are the same as the ones that you created in the IAM dashboard. You may need to generate new keys\n${
-                  err.stack
-                }`
-              );
+              alert(`Your request for a signed URL failed with a status 403.`);
             } else {
               alert(`ERROR: ${err.status}\n ${err.stack}`);
             }
           });
       };
+
+      setEditorRef = (editor) => this.editor = editor
     
     render(){
-        const{url, isUploading} = this.state;
+        const{isUploading} = this.state;
         return (
             <div className='Design'>
                 <ButtonGroup>
@@ -82,7 +78,34 @@ class Pictures extends Component {
                     <Button bsPrefix='active-customize-step-btn'>5</Button>
                 </ButtonGroup>
                 <div className='custom-board-image-div'>
-                    <h5 className='custom-step-name'>Graphics</h5>
+                    {!this.props.graphic
+                    ?(<div>
+                          <h5 className='custom-step-name'>Upload a Graphic</h5>
+                          <img src={this.props.design.image} alt='design' className='selected-board-top' />
+                          <img src={this.props.design.image} alt='design' className='selected-board-bottom' />
+                          <img src={this.props.grip.image} alt='grip' className='selected-board-top' />
+                          <img src={this.props.trucks.image} alt='trucks' className={`selected-trucks-top-front-${this.props.design.name}`} />
+                          <img src={this.props.trucks.image} alt='trucks' className={`selected-trucks-top-back-${this.props.design.name}`} />
+                          <img src={this.props.trucks.image} alt='trucks' className={`selected-trucks-bottom-front-${this.props.design.name}`} />
+                          <img src={this.props.trucks.image} alt='trucks' className={`selected-trucks-bottom-back-${this.props.design.name}`} />
+                          <img src={this.props.wheels.image} alt='wheels' className={`selected-wheels-top-front-${this.props.design.name}`} />
+                          <img src={this.props.wheels.image} alt='wheels' className={`selected-wheels-top-back-${this.props.design.name}`} />
+                          <img src={this.props.wheels.image} alt='wheels' className={`selected-wheels-bottom-front-${this.props.design.name}`} />
+                          <img src={this.props.wheels.image} alt='wheels' className={`selected-wheels-bottom-back-${this.props.design.name}`} />
+                      </div>)
+                    :(<div>
+                          <h5 className='custom-step-name'>Graphic</h5>
+                          <div>
+                              <img src={this.props.design.image} alt='design' className='selected-board' />
+                              <img src={this.props.graphic} alt='graphic' className='uploaded-graphic'/>
+                              <img src={`https://s3-us-west-1.amazonaws.com/old-dog-new-trick-longboards-bucket/${this.props.design.name}_board_outline.png`} alt='outline' className='board-outline'/>
+                          </div>
+                          <img src={this.props.trucks.image} alt='trucks' className={`selected-trucks-front-${this.props.design.name}`} />
+                          <img src={this.props.trucks.image} alt='trucks' className={`selected-trucks-back-${this.props.design.name}`} />
+                          <img src={this.props.wheels.image} alt='wheels' className={`selected-wheels-front-${this.props.design.name}`} />
+                          <img src={this.props.wheels.image} alt='wheels' className={`selected-wheels-back-${this.props.design.name}`} />
+                      </div>)
+                    }
                 </div>
                 <div className='custom-board-buttons-div'>
                     <ButtonGroup bsPrefix='custom-btn-group'>
@@ -93,13 +116,16 @@ class Pictures extends Component {
                             {({getRootProps, getInputProps}) => (
                             <Button bsPrefix='custom-btn' {...getRootProps()}>
                                 <input {...getInputProps()} />
-                                {isUploading ? <GridLoader /> : <span>Upload Graphic</span>}
+                                {isUploading ? <span>Loading...</span> : <span>Upload Graphic</span>}
                             </Button>
                         )}
                         </Dropzone>
                         <Button bsPrefix='custom-btn'>Add to Cart</Button>
                     </ButtonGroup>
                 </div>
+                <p>*Note that all images may not fit onto the the longboard surface. It is recommended that you use images that have been cropped to work with your
+                    selected longboard shape.
+                </p>
             </div>
         )
     }
